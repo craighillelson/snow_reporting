71eca11fb4fe2c75565a00b5090bd42ed46e74b2
craighillelson <craighillelson@gmail.com>
2020-02-19 17:51:33 -0500

    update avg_res_time_days_dec.py

--- avg_res_time_days_dec.py
+++ avg_res_time_days_dec.py
@@ -1,31 +1,41 @@
 """ __doc__ """
 
-print('how many days')
-days = int(input())
-
-print('how many hours')
-hrs = int(input())
-
-print('how many minutes')
-mins = int(input())
-
-print('\n')
-
-print(f'{days} days {hrs} hours {mins} minutes')
-
 mins_hr = 60
 mins_day = 24 * 60
-print(f'minutes per day: {mins_day}')
-tot_mins = hrs * mins_hr + mins
-print(f'total minutes: {tot_mins}')
-print('\n')
 
-mins_dec = (hrs * mins_hr + mins) / mins_day
-mins_dec_fmt = float('{0:.2f}'.format(mins_dec))
-print(f'percentage of a day: {mins_dec_fmt}')
-print('\n')
+client_res_time = {}
 
-days_dec = days + mins_dec_fmt
-print(f'days plus minutes as percentage of a day {days_dec}')
+RTN = lambda: '\n'
 
-print('\n')
+print(RTN())
+
+while True:
+    print('client name (blank to quit)')
+    client = input()
+    if client == '':
+        break
+    print('how many days')
+    days = int(input())
+    print('how many hours')
+    hrs = int(input())
+    if hrs > 24:
+        hrs = int(input('please enter a number less than 24\n'))
+    else:
+        pass
+    print('how many minutes')
+    mins = int(input())
+    if mins > 60:
+        mins = int(input('please enter a number less than 60\n'))
+    else:
+        pass
+    tot_mins = hrs * mins_hr + mins
+    mins_dec = (hrs * mins_hr + mins) / mins_day
+    mins_dec_fmt = float('{0:.2f}'.format(mins_dec))
+    days_dec = days + mins_dec_fmt
+    client_res_time[client] = days_dec
+
+print('client - resolve time (days)')
+for client, res_time in client_res_time.items():
+    print(f'{client} {res_time} days')
+
+print(RTN())
