5bf72cfd7a155e4654e325553add428642c47cc6
craighillelson <craighillelson@gmail.com>
2020-02-19 18:05:06 -0500

    update menu.py

--- menu.py
+++ menu.py
@@ -1,71 +1,26 @@
 """ __doc__ """
 
-# menu
-
-import os
-
 RTN = lambda: '\n'
 
-entries = os.listdir()
-choices_lst = []
-options = []
-choices_dct = {}
-alphabet = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-]
+options = {
+    'a': 'avg_res_time_days_dec.py',
+    'b': 'companies_by_num_tasks_perc.py',
+}
 
-for file in sorted(entries):
-    if '.py' in file and 'functions' not in file and 'menu' not in file:
-        choices_lst.append(file)
+def switch_case(a, b):
+    """ switch case statement """
+    a
+    return a.get(b, 'nothing')
 
-i = 0
 
-for choice in choices_lst:
-    choices_dct[f'{alphabet[i]}.'] = choices_lst[i]
-    options.append(alphabet[i])
-    i += 1
+def open_file(a):
+    """ """
+    exec(open(a).read())
 
-print(RTN())
+print('a - average resolution time in days')
+print('b - companies by number of incidents created')
 
 while True:
-    try:
-        print('What is the name of the file you uploaded?')
-        file_name = input()
-        print(RTN())
-        print('please make a selection')
-        for letter, script in choices_dct.items():
-            print(letter, script)
-        usr_choice = input()
-        if usr_choice not in options:
-            print('not one of the options\n')
-        else:
-            break
-    except ValueError:
-        print('invalid input')
-
-print(RTN())
+    usr_choice = input()
+    open_file(switch_case(options, usr_choice))
+    break
