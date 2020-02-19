70193f66976029e7cfa4d7541a58d79783215f94
craighillelson <craighillelson@gmail.com>
2020-02-19 18:07:30 -0500

    update menu.py

--- menu.py
+++ menu.py
@@ -5,6 +5,9 @@ RTN = lambda: '\n'
 options = {
     'a': 'avg_res_time_days_dec.py',
     'b': 'companies_by_num_tasks_perc.py',
+    'c': 'empty_account.py',
+    'd': 'mult_short_desc.py',
+    'e': 'search_for_term.py',
 }
 
 def switch_case(a, b):
@@ -19,6 +22,9 @@ def open_file(a):
 
 print('a - average resolution time in days')
 print('b - companies by number of incidents created')
+print('c - find incidents where account is empty')
+print('d - mult short descriptions')
+print('e - search for term')
 
 while True:
     usr_choice = input()
