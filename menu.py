f1b22491b41ed1595c9e4461571ca14a7f5574ac
craighillelson <craighillelson@gmail.com>
2020-02-23 08:10:18 -0500

    update menu.py
    added % change in incidents week over week as an option

--- menu.py
+++ menu.py
@@ -4,10 +4,11 @@ RTN = lambda: '\n'
 
 options = {
     'a': 'avg_res_time_days_dec.py',
-    'b': 'companies_by_num_tasks_perc.py',
-    'c': 'empty_account.py',
-    'd': 'mult_short_desc.py',
-    'e': 'search_for_term.py',
+    'b': 'inc_cre_wk_ov_wk.py',
+    'c': 'companies_by_num_tasks_perc.py',
+    'd': 'empty_account.py',
+    'e': 'mult_short_desc.py',
+    'f': 'search_for_term.py',
 }
 
 def switch_case(a, b):
@@ -21,10 +22,11 @@ def open_file(a):
     exec(open(a).read())
 
 print('a - average resolution time in days')
-print('b - companies by number of incidents created')
-print('c - find incidents where account is empty')
-print('d - mult short descriptions')
-print('e - search for term')
+print('b - % change in incidents created week over week')
+print('c - companies by number of incidents created')
+print('d - find incidents where account is empty')
+print('e - mult short descriptions')
+print('f - search for term')
 
 while True:
     usr_choice = input()
