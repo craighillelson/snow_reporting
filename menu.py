0c698e9b19af766926a41bf7b232fbdc158d91de
Craig Hillelson <craighillelson@gmail.com>
2020-02-26 09:19:20 -0500

    update menu.py

--- menu.py
+++ menu.py
@@ -5,25 +5,26 @@ from datetime import timedelta
 
 RTN = lambda: '\n'
 
-weekdays = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday',
-]
+# weekdays = [
+#     'Monday',
+#     'Tuesday',
+#     'Wednesday',
+#     'Thursday',
+#     'Friday',
+#     'Saturday',
+#     'Sunday',
+# ]
 
 options = {
-    'a': 'avg_res_time_days_dec.py',
-    'b': 'deltas_weekly.py',
-    'c': 'deltas_spec_dates.py',
-    'd': 'companies_by_num_tasks_perc.py',
-    # 'd': 'empty_account.py',
-    # 'e': 'mult_short_desc.py',
-    # 'f': 'search_for_term.py',
-    # 'g': 'next_fri.py',
+    'a': 'avg_res_time_days_dec.py', # works off user input
+    'b': 'deltas_weekly.py', # works off incs_cre_wkly.csv
+    'c': 'deltas_spec_dates.py', # works off incs_cre_wkly.csv
+    'd': 'num_perc_incs_by_co.py', # works off raw_data.csv
+    'e': 'mult_short_desc.py',
+    # 'f': 'empty_account.py',
+    # 'g': 'mult_short_desc.py',
+    # 'h': 'search_for_term.py',
+    # 'i': 'next_fri.py',
 }
 
 def switch_case(a, b):
@@ -43,10 +44,12 @@ print('please make a selection from the options below or \'enter\' to quit')
 print('a - calculate average resolution time in days')
 print('b - % change in incidents created week over week')
 print('c - % change in incidents created between two dates')
-print('d - companies by number of incidents created')
-# print('e - find incidents where account is empty')
-# print('f - mult short descriptions')
-# print('g - search for term')
+print('d - companies by number and percentage of incidents created')
+print('e - occurances of short descriptions by account ytd')
+# print('f - find incidents where account is empty')
+# print('g - mult short descriptions')
+# print('h - search for term')
+# print('i - assemble report')
 
 usr_choice = str(input())
 
