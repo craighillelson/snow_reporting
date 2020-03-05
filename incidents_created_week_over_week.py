a35cbf4478a3396f3240513a6e2b38519278df58
Craig Hillelson <craighillelson@gmail.com>
2020-03-04 20:11:57 -0500

    update incidents_created_week_over_week.py
    restructured code to make it more readable

--- incidents_created_week_over_week.py
+++ incidents_created_week_over_week.py
@@ -1,10 +1,7 @@
 """ __doc__ """
 
 import csv
-
-PERC_CHANGE_DCT = {}
-
-# HEADERS = # name headers
+import functions
 
 def usr_input_date(a, b):
     """ get user input """
@@ -22,23 +19,26 @@ def usr_input_incs(a, b):
     return b
 
 
-def fmt_perc():
+def fmt_perc(a, b, c):
     """ format percentage """
-    perc_change = diff / tix_last_wk * 100
-    # print(perc_change)
-    perc_change_fmt = '{0:.2f}'.format(perc_change)+'%'
+    perc_change = a / b * 100
+    c = '{0:.2f}'.format(perc_change)+'%'
 
-    return perc_change_fmt
+    return c
 
 
-def add_to_dct():
-    """ """
-    PERC_CHANGE_DCT[wk_end_date] = perc_change_fmt
+def add_to_dct(a, b, c):
+    """ add to dictionary """
+    a[b] = c
+
+    return a
 
 
 def write_to_csv(name_of_file, HEADERS, dct):
     """ write dictionary to csv """
-    with open(name_of_file, "w") as out_file:
+    import csv
+
+    with open(name_of_file, 'w') as out_file:
         out_csv = csv.writer(out_file)
         out_csv.writerow(HEADERS)
         for k, v in dct.items():
@@ -46,25 +46,37 @@ def write_to_csv(name_of_file, HEADERS, dct):
             out_csv.writerow(keys_values)
 
 
+print(functions.RTN())
 wk_end_date = usr_input_date('enter week ending date (YYYY-MM-DD)',
                              'wk_end_date')
+
+print(functions.RTN())
 tix_last_wk = usr_input_incs('how many incidents were created last week?',
                              'tix_last_wk')
+
+print(functions.RTN())
 tix_this_wk = usr_input_incs('how many incidents were created this week?',
                              'tix_this_wk')
+print(functions.RTN())
 
 diff = tix_this_wk - tix_last_wk
-print('diff:', diff)
+print(f'difference: {diff}')
+PERC_CHANGE_DCT = {}
 if diff < 0:
-    perc_change_fmt = fmt_perc()
-    add_to_dct()
-    print(perc_change_fmt)
+    perc_change_fmt = fmt_perc(diff, tix_last_wk, 'perc_change_fmt')
+    print(f'percentage change: {perc_change_fmt}')
+    add_to_dct(PERC_CHANGE_DCT, wk_end_date, perc_change_fmt)
 else:
-    perc_change_fmt = fmt_perc()
-    add_to_dct()
-    print('+' + perc_change_fmt)
+    perc_change_fmt = fmt_perc(diff, tix_last_wk, 'perc_change_fmt')
+    perc_change_fmt = '+' + perc_change_fmt
+    print(f'percentage change: {perc_change_fmt}')
+    add_to_dct(PERC_CHANGE_DCT, wk_end_date, perc_change_fmt)
+
+print(functions.RTN())
 
 write_to_csv('perc_change_incidents_created_week_over_week.csv',
              ['week','perc_change'], PERC_CHANGE_DCT)
 
 print('"perc_change_incidents_created_week_over_week.csv" exported succesfully')
+
+print(functions.RTN())
