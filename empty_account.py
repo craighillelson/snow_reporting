c3c97e7539cd74a65d1531974327d062a6d615d0
Craig Hillelson <craighillelson@gmail.com>
2020-03-05 13:48:33 -0500

    update empty_account.py

--- empty_account.py
+++ empty_account.py
@@ -1,13 +1,32 @@
 """ __doc__ """
 
+import codecs
+import csv
+from collections import namedtuple
 import functions
 
-# import csv and populate a dictionary with its contents
-functions.populate_dct()
+INCIDENTS = {}
 
-# company field is empty
+with codecs.open('raw_data.csv', 'r', encoding='utf-8',
+            errors='ignore') as csv_file:
+    F_CSV = csv.reader(csv_file)
+    COLUMN_HEADINGS = next(F_CSV)
+    CSV_ROW = namedtuple('Row', COLUMN_HEADINGS)
+    for rows in F_CSV:
+        row = CSV_ROW(*rows)
+        if row.company == "":
+            INCIDENTS[row.number] = row.company, row.short_description
+
 print(functions.RTN())
-print("company is empty".upper())
-for k, v in functions.DESCRIPTIONS_DCT.items():
-    if not v[0]:
-        print(k, v[1])
+
+if not INCIDENTS:
+    print('no results')
+else:
+    print('company is empty'.upper())
+    for num, company_short_description in INCIDENTS.items():
+        company = company_short_description[0]
+        short_description = company_short_description[1]
+        if not company:
+            print(num, short_description)
+
+print(functions.RTN())
