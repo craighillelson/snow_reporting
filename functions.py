0fd80f5eda1f3cfac00f8d03ccfb53451af26848
Craig Hillelson <craighillelson@gmail.com>
2020-03-05 19:00:40 -0500

    update functions.py

--- functions.py
+++ functions.py
@@ -6,8 +6,13 @@ from collections import namedtuple
 
 RTN = lambda: '\n'
 
+def format_percentage(a, b):
+    per = float(a / b) * 100
+    per_fmt = '{0:.2f}'.format(per)+'%'
+    return per_fmt
+
+
 def open_csv(a, b):
-    """ open csv and populate dictionary with its contents """
     with codecs.open('raw_data.csv', 'r', encoding='utf-8',
                 errors='ignore') as csv_file:
         F_CSV = csv.reader(csv_file)
@@ -20,61 +25,10 @@ def open_csv(a, b):
                 b[row.number] = row.company, row.short_description
 
 
-def format_days(a, b):
-    """ format days as floats to the second decimal """
-    b = '{0:.2f}'.format(a)
-
-    return b
-
-
-def print_headers():
-    """ print headers """
-    print('number,company,short description')
-    print(RTN())
-
-
-def print_break():
-    """ print visual divider """
-    print('*' * 10)
-
-
-def format_percentage(a, b):
-    """ format floats as percentages """
-    per = float(a / b) * 100
-    per_fmt = '{0:.2f}'.format(per)+'%'
-    return per_fmt
-
-
-def write_to_csv(a, b, c):
-    """ write dictionary to csv """
-    import csv
-
-    # define HEADERS
-    HEADERS = a
-
-    with open(b, 'w') as out_file:
-        out_csv = csv.writer(out_file)
-        out_csv.writerow(HEADERS)
-        for k, v in c.items():
-            keys_values = (k, v)
-            out_csv.writerow(keys_values)
-
-
-def count_words(a):
-    """ count words """
-    a = short_desc_str.split()
-
-    for w in a:
-        wordfreq.append(a.count(w))
-
-    # zip lists
-    for word_count in zip(a, wordfreq):
-        dct[word_count[0]] = word_count[1]
-
-    # output results
-    print('word, count')
-    for word, count in sorted(dct.items(), key=lambda x: x[1], reverse=True):
-        print(word, count)
+def switch_case(a, b):
+    """ switch case statement """
+    a
+    return a.get(b, 'nothing')
 
 
 def total_tasks(a):
